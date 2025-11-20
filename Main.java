86f5e1c9c2e15143ed83004390fdba8b3f91ce79
Phil <87224443+phil-passon@users.noreply.github.com>
2025-11-20 14:34:39 +0100

    Implement PalindromeChecker class

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class PalindromeChecker {
+
+    public static boolean isPalindrome(String wordToBeChecked){
+        char[] word = wordToBeChecked.toCharArray();
+        boolean result = false;
+
+        if (compare(word, mirrorArray(word))){
+            result = true;
+        }
+        return result;
+    }
+
+    private static boolean compare(char[] source, char[] target) {
+        boolean result = false;
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] != target[i]) {
+                result = false;
+                break;
+            } else if (source[i] == target[i]) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    static char[] mirrorArray(char[] charArray) {
+        char[] resultArray =  new char[charArray.length];
+        int lastIndex = charArray.length-1;
+        for (int i = 0; i < charArray.length; i++) {
+            resultArray[lastIndex--] = charArray[i];
+        }
+        return resultArray;
+    }
+}
